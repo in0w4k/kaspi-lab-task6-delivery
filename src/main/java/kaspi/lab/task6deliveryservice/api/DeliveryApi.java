e450cb9d94140cf18d335c7417f60e9b79f69d39
Ilya Lishkevich <in0w4.k+github@gmail.com>
2026-01-19 18:23:57 +0500

    feat: task 9 updates

--- src/main/java/kaspi/lab/task6deliveryservice/api/DeliveryApi.java
+++ src/main/java/kaspi/lab/task6deliveryservice/api/DeliveryApi.java
@@ -2,19 +2,20 @@ package kaspi.lab.task6deliveryservice.api;
 
 import kaspi.lab.task6deliveryservice.dto.DeliveryRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/delivery")
 public class DeliveryApi {
     @PostMapping
-    public ResponseEntity<String> delivery(@RequestBody DeliveryRequest request) {
-        String response = "Delivery created successfully! Product ID: " + request.getProductId() + ", Address: " + request.getAddress();
-        System.out.println(response);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<String> delivery(@RequestBody DeliveryRequest request) {
+        return Mono.fromCallable(() -> {
+            String response = "Delivery created successfully! Product ID: " +
+                    request.getProductId() + ", Address: " + request.getAddress();
+            System.out.println(response);
+            return response;
+        });
     }
 }
